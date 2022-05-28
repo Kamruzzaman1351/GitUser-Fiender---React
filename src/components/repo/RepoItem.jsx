9eecb81a579879e6ce9a745c23433567bb032fc9
kamruzzaman1351 <mdrashed1351@gmail.com>
2022-05-28 15:09:27 +0600

    Populeted the repo item with fetch data

--- src/components/repo/RepoItem.jsx
+++ src/components/repo/RepoItem.jsx
@@ -1,31 +1,36 @@
+import PropsType from "prop-types"
 import {FaLink, FaEye, FaStar, FaInfo, FaUtensils} from "react-icons/fa"
-function RepoItem() {
+function RepoItem({repo: {html_url,description,watchers_count,stargazers_count,forks_count,open_issues}}) {
   return (
     <div className='mb-2 rounded-md card bg-gray-800 hover:bg-gray-900'>
         <div className='card-body'>
             <h3 className='mb-2 text-xl font-semibold'>
-                <a href="#" target="_blank" rel='noreferrer'>
-                    <FaLink className='inline mr-1' /> Link Goes Here
+                <a href={html_url} target="_blank" rel='noreferrer'>
+                    <FaLink className='inline mr-1' /> {html_url}
                 </a>
             </h3>
-            <p className='mb-3'> Repo Description </p>
+            <p className='mb-3'> {description} </p>
             <div>
                 <div className='mr-2 badge badge-info badge-lg'>
-                    <FaEye className='mr-2' /> Watcher Count
+                    <FaEye className='mr-2' />{watchers_count}
                 </div>
                 <div className='mr-2 badge badge-success badge-lg'>
-                    <FaStar className='mr-2' /> Stargazers Count
+                    <FaStar className='mr-2' /> {stargazers_count}
                 </div>
                 <div className='mr-2 badge badge-error badge-lg'>
-                    <FaInfo className='mr-2' /> Open Issues
+                    <FaInfo className='mr-2' /> {open_issues}
                 </div>
                 <div className='mr-2 badge badge-warning badge-lg'>
-                    <FaUtensils className='mr-2' /> Forks Count
+                    <FaUtensils className='mr-2' /> {forks_count}
                 </div>
             </div>
         </div>
     </div>
   )
+}
+
+RepoItem.propsType = {
+  repo: PropsType.object.isRequired,
 }
 
 export default RepoItem
--- src/components/repo/UserRepo.jsx
+++ src/components/repo/UserRepo.jsx
@@ -1,14 +1,17 @@
-import RepoItem from "./RepoItem"
+import RepoItem from "./RepoItem";
+import { useContext } from "react"
+import GithubContext from "../../context/github/GithubContext";
 function UserRepo() {
+  const{repos} = useContext(GithubContext);
   return (
     <div className='rounded-lg shadow-lg card bg-base-100'>
         <div className='card-body'>
             <h2 className='text-3xl my-4 font-bold card-title'>
                 Latest Respositories
             </h2>
-            <RepoItem />
-            <RepoItem />
-            <RepoItem />
+            {repos.map((repo) => (
+              <RepoItem key={repo.id} repo={repo}/>
+            ))}
         </div>
     </div>
 
